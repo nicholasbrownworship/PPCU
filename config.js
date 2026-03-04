e1f659ca23b5612ac324b9ad15c7329501daf849
nicholasbrownworship <nicholasbrownworship@gmail.com>
2026-03-03 21:44:54 -0600

    Update API_KEY and SHEET_ID in config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@
 const PPCU_CONFIG = {
   // Your Google Sheets API key
   // Get one at: https://console.cloud.google.com/
-  API_KEY: "YOUR_API_KEY_HERE",
+  API_KEY: "AIzaSyBMOEPFMlODdXWn4rbd4snTznqIJh-jyE4",
 
   // The ID from your Google Sheet URL
   // e.g. https://docs.google.com/spreadsheets/d/THIS_PART_HERE/edit
-  SHEET_ID: "YOUR_SHEET_ID_HERE",
+  SHEET_ID: "1scmgKWM8p0-CaG3fDayY9LAlAhqQnAoBI5HhvJ42iww",
 
   // The name of the tab in your Google Sheet (default: Sheet1)
   SHEET_TAB: "Results",
